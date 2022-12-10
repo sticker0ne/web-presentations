c0c830f7ad75dcd6482d47df507f972486d7120c
spmirtov <spmirtov@yandex.team.ru>
2022-12-10 19:53:09 +0400

    Rebuild gh pages 2

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
 
+import "primeflex/primeflex.css";
 import "primevue/resources/themes/lara-light-purple/theme.css";
 import "primevue/resources/primevue.min.css";
-import "primeflex/primeflex.css";
 
 const app = createApp(App);
 
